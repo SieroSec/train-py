ca89eee34d03b9842dd8100e1fc86bb808af4316
SieroSec <SieroSec@gmail.com>
2020-12-02 19:10:13 +0100

    Super

--- web/__init__.py
+++ web/__init__.py
@@ -1,7 +1,62 @@
+
+#
+# python3 -m pip install --user --upgrade pip
+# python3 -m pip install --user virtualenv
+# create venv: python3 -m venv env
+# source env/bin/activate
+
+# FROM VIDEO:
+# pipenv --python python3.9 install flask
+# pipenv shell 
+# pipenv install psycopg2-binary Flask-Migrate Flask-SQLAlchemy
 # flask run --host=0.0.0.0 --port=3000
+
+# Docker:
+# dockerd
+# sudo docker ps
+# sudo docker exec -i postgres psql postgres -U pguser -c "CREATE DATABASE notes;"
+# sudo docker exec -i postgres psql postgres -U pguser -c "ALTER USER pguser PASSWORD 'pgpassword';"
+
+# Postgres:
+# sudo systemctl start postgresql@13-main
+# sudo systemctl enable postgresql@13-main
+#
+# sudo docker rm --force postgres
+# sudo docker volume create pg-data
+# sudo docker run --name postgres2 POSTGRES_USER=pguser -e POSTGRES_PASSWORD=pgpassword -d postgres -v "pg-data:/var/lib/postgresql/data" -e PGDATA=/var/lib/postgresql/data/pgdata
+# 
+# sudo docker volume create pg-data
+# sudo docker run -d \
+# --name postgres \
+# -e POSTGRES_USER=pguser \
+# -e POSTGRES_PASSWORD=pgpassword \
+# -e POSTGRES_DB=notes \
+# -e PGDATA=/var/lib/postgresql/data/pgdata \
+# -v "pg-data:/var/lib/postgresql/data" \
+# -p "5432:5432" \
+# --restart always \
+# postgres:13.1-alpine
+#
+# Connect to Postgres in Docker Container
+# docker exec -it [container_name] psql -U [postgres_user] [db_name]
+# docker exec -it postgres psql -U pguser notes
+# 
+# Connect to the database as the postgres user type:
+# create database [db_name];
+# \c [db_name]
+# create schema [db_schema_name]
+# create table [table_name] ([field_names] [values])
+####################
+#
+# pipenv install --dev python-dotenv
+# To activate this project's virtualenv, run pipenv shell.
+# Alternatively, run a command inside the virtualenv with pipenv run.
+
+
 
 import os
 from flask import Flask
+from flask_migrate import Migrate
 
 def create_app(test_config=None):
     app = Flask(__name__)
@@ -11,6 +66,10 @@ def create_app(test_config=None):
         app.config.from_pyfile('config.py', silent=True)
     else:
         app.config.from_mapping(test_config)
+    
+    from .models import db
+    db.init_app(app)
+    migrate = Migrate(app,db)
         
     return app
 
--- web/config.py
+++ web/config.py
@@ -2,8 +2,8 @@ import os
 
 db_host = os.environ.get('DB_HOST', default='localhost')
 db_name = os.environ.get('DB_NAME', default='notes')
-db_user = os.environ.get('DB_USERNAME', default='dbuser')
-db_password = os.environ.get('DB_PASSWORD', default='db_password')
+db_user = os.environ.get('DB_USERNAME', default='pguser')
+db_password = os.environ.get('DB_PASSWORD', default='pgpassword')
 db_port = os.environ.get('DB_PORT', default='5432')
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
