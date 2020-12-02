f80fc0474a351c0e51a5d0ab0e9c8c7d839ebf22
SieroSec <SieroSec@gmail.com>
2020-12-02 23:06:22 +0100

    sqlalchemy Does not work

--- web/models.py
+++ web/models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+#from mistune import markdown
+
 
 db = SQLAlchemy()
 
@@ -18,5 +20,8 @@ class Note(db.Model):
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     
+    # @property
+    # def body_html(self):
+    #     return markdown(self.body)
 
 
