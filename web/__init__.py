0f898cebbdd5aeae4c79ae54e55ccc79b07c404e
SieroSec <SieroSec@gmail.com>
2020-12-02 21:15:36 +0100

    jinja tpls

--- web/__init__.py
+++ web/__init__.py
@@ -49,7 +49,7 @@
 # 
 # #psql -h localhost -p 5432 -U postgress testdb
 ####################
-# postgres show tables: \dt
+# postgres show tables: \dt OR \dt+ (describe tables)
 #
 #
 #
@@ -63,6 +63,7 @@
 # flask db init
 # flask db migrate
 # flask db upgrade
+#
 # git log
 # git status
 # git add --all .
@@ -74,8 +75,9 @@
 
 
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request, session, flash
 from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash
 
 def create_app(test_config=None):
     app = Flask(__name__)
@@ -86,10 +88,76 @@ def create_app(test_config=None):
     else:
         app.config.from_mapping(test_config)
     
-    from .models import db
+    from .models import db, User
     db.init_app(app)
     migrate = Migrate(app,db)
+    
+    @app.route('/sign_up', methods=('GET','POST'))
+    def sign_up():
+        if request.method == 'POST':
+            username = request.form['username']
+            password = request.form['password']
+            error = None
+            
+            if not username:
+                error = 'Username is required.'
+            elif not password:
+                error = 'Password is required.'
+            elif User.query.filter_by(username=username).first():
+                error = 'Username is already taken.'
+            
+            if error is None:
+                user = User(username=username, password=password)
+                # user = User(username=username, password=generate_password_hash(password))
+                db.session.add(user)
+                db.session.commit()
+                flash("Successfully signed up! Please log in.", 'success')
+                return redirect(url_for('log_in'))
+
+            flash(error, 'error')
+        return render_template('sign_up.html')
+
+    @app.route('/log_in', methods=('GET','POST'))
+    def log_in():
+        if request.method == 'POST':
+            username = request.form['username']
+            password = request.form['password']
+            error = None
         
+            user = User.query.filter_by(username=username).first()
+
+            #if not user or not check_password_hash(user.password, password):
+            if not user or not user.password:
+                error = 'username or Password are incorrect.'
+            
+            if error is None:
+                session.clear()
+                session['user_id'] = user.id
+                return redirect(url_for('index'))
+            
+            flash(error, 'error')
+        
+        return render_template('log_in.html')
+    
+    @app.route('/log_out', methods=('GET','DELETE'))
+    def log_out():
+        if request.method == 'GET':
+            session.clear()
+            flash('Successfully logged out. GET method', 'success')
+            return redirect(url_for('log_in'))
+        elif request.method == 'DELETE':
+            session.clear()
+            flash('Successfully logged out. DELETE method', 'success')
+            return redirect(url_for('log_in'))
+        else :
+            session.clear()
+            flash('Successfully logged out. UNKNOWN method', 'success')
+            return redirect(url_for('log_in'))
+    
+    @app.route('/')
+    def index():
+        return 'Index'
+    
     return app
 
 
